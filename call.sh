438173663c5b2170cd2d13c3ef20da4b6a33571c
wisnusaputra123 <52888512+wisnusaputra123@users.noreply.github.com>
2019-07-15 08:37:21 +0700

    Update and rename call.php to call.sh

--- call.sh
+++ call.sh
@@ -1,5 +1,33 @@
-<?php
-// Copyright : W
+  
+
+#!/bin/bash
+#version 1.0
+
+# Variables
+b='\033[1m'
+u='\033[4m'
+bl='\E[30m'
+r='\E[31m'
+g='\E[32m'
+bu='\E[34m'
+m='\E[35m'
+c='\E[36m'
+w='\E[37m'
+endc='\E[0m'
+enda='\033[0m'
+blue='\e[1;34m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+
+figlet Wisnu | lolcat
+figlet Saputra | lolcat
+echo -e $b "_________________________________________________________"
+echo -e  "instagram : wisnu_as123 $white         " |lolcat
+echo -e  "whatsapp  : 085780496494 $white   " |lolcat
+echo -e  "Contact   : username123@gmail.com $white " |lolcat
+echo -e $b "_________________________________________________________"
+
+/ Copyright : W
 // Limit 3x Telpon Setiap Satu Nomor
 function send($phone){
 	$config['headers'] = explode("\n",'Host: www.airbnb.co.id
