4fe5c22f8d0fc906ade91203264de9f8c55aee7c
wisnusaputra123 <52888512+wisnusaputra123@users.noreply.github.com>
2019-07-15 07:55:27 +0700

    Update tema.php

--- tema.php
+++ tema.php
@@ -1,5 +1,27 @@
-<?php
-cowsay -f eyes "username123 | lolcat
-toilet -f standard "Wisnu saputra" -F gay
-neofetch
-date | lolcat
+
+#!/bin/bash
+#version 1.0
+
+# Variables
+b='\033[1m'
+u='\033[4m'
+bl='\E[30m'
+r='\E[31m'
+g='\E[32m'
+bu='\E[34m'
+m='\E[35m'
+c='\E[36m'
+w='\E[37m'
+endc='\E[0m'
+enda='\033[0m'
+blue='\e[1;34m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+
+figlet Wisnu | lolcat
+figlet Saputra | lolcat
+echo -e  "_____________________________________________________________"
+echo -e  "Tools    : Wisnu-Tools-Installer $white         " |lolcat
+echo -e  "Creadby  : Wisnu_Saputra-123 $white   " |lolcat
+echo -e  "Contact  : username123@gmail.com $white " |lolcat
+echo -e  "_____________________________________________________________"
