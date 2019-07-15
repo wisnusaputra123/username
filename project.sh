7be80e4a31956feb72a9fd001f1ea886e4c2ec32
wisnusaputra123 <52888512+wisnusaputra123@users.noreply.github.com>
2019-07-15 12:02:30 +0700

    Update project.sh

--- project.sh
+++ project.sh
@@ -26,48 +26,4 @@ echo -e  "whatsapp  : 085780496494 $white   " |lolcat
 echo -e  "Contact   : username123@gmail.com $white " |lolcat
 echo -e $b "_______________________________________________"
 
-###################################################
-# CTRL + C
-###################################################
 
-trap ctrl_c INT
-ctrl_c() {
-clear
-echo -e $red"[#]> (Ctrl + C ) Detected, Trying To Exit ... "
-echo -e $cyan"[#]> Thanks"
-sleep 1
-echo ""
-echo -e $white"[#]> see you gaes :)..."
-sleep 1
-exit
-}
-
-lagi=1
-while [ $lagi -lt 6 ];
-do
-echo ""
-echo -e $b "1.  python2${endc}";
-echo -e "============================" | lolcat
-echo -e $r "2.  Matrix${endc}";
-echo -e "============================" | lolcat
-echo -e $g "3.  php${endc}";
-echo -e "============================" | lolcat
-echo -e $c "4.  git${endc}";
-echo -e "============================" | lolcat
-echo -e $r"5.  ruby${endc}";
-echo -e "============================" | lolcat
-echo -e $r"6.  Exit${endc}";
-echo ""
-echo -e "╭─wisnu" |lolcat
-read -p "╰─#" pil;
-
-
-
-6) echo "created by : wisnu-saputra" | lolcat
-exit
-;;
-
-*) echo "sorry, pilihan yang anda cari tidak ada"
-esac
-done
-done
